6989d84f316565493269055e2a3c31f0ff81b335
REMILI Brahim <mohamedremili5000@gmail.com>
2026-06-14 13:27:42 +0100

    mmmm

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let NormalString = "Hello im ibrahim";
 
 // Count : Method 01
 function count(NormalString) {
-    OnlyChar = NormalString.replace(/\s+/g,'') 
+    OnlyChar = NormalString.replace(/\s+/g, '')
     console.log('Count = ', OnlyChar.length)
 }
 count(NormalString)
@@ -10,26 +10,47 @@ count(NormalString)
 
 // Count :  Method 02 
 function count(NormalString) {
-    for(i in NormalString) {
+    for (i in NormalString) {
     }
-    console.log('Count = ', i-1)
+    console.log('Count = ', i - 1)
 }
 count(NormalString)
 
 // Revers
 function revers(NormalString) {
     let ReversedString = ""
-    for( i of NormalString){
-        
-    ReversedString = i + ReversedString
+    for (i of NormalString) {
+
+        ReversedString = i + ReversedString
     }
     console.log('Revers : ' + ReversedString)
 }
 revers(NormalString)
 
 // Capitalize
-function capitalize(NormalString){
+function capitalize(NormalString) {
     let BigWord = NormalString.toUpperCase();
     console.log('Capitalize : ' + BigWord)
 }
 capitalize(NormalString)
+
+// max value and min value
+const Numbers = [0, 1, 6, 3, 4, 5];
+
+function Values(Numbers){
+    
+    let min = null
+    for (i = 0; i >= Numbers.length; i++) {
+        for (j = 0; j > Numbers.length; j++) {
+            if (j > Numbers[i]) {
+              console.log("yes") 
+               
+            }else 
+                console.log("no")
+
+
+        }
+    }
+    console.log(Numbers[2])
+}
+Values(Numbers)
